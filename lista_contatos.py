fd9881541cb9a3ad95ace2842cf69dd76b88eca3
Lucas Bueloni Angeli Romani <80349346+lucasbueloni@users.noreply.github.com>
2026-07-21 11:12:27 -0300

    Refactor contact management functions and UI

--- lista_contatos.py
+++ lista_contatos.py
@@ -1,109 +1,102 @@
-def adicionar_contatos(nome, telefone, email, lista_contatos):
-    lista = {"Nome": nome, "Telefone": telefone, "Email": email, "Favorito": False}
-    lista_contatos.append(lista)
-    print("\nContato adicionado com sucesso")
+def visualizar_contatos(lista_contatos):
+    for indice, contato in enumerate (lista_contatos, start=1):
+        favorito = "★" if contato["Favorito"] else "X"
+        print(f'{indice}.  {contato["Nome"]} - {contato["Telefone"]} - {contato["Email"]} - {favorito}')
     return
 
-def visualizar_contatos(lista_contatos):
-    for indice, lista in enumerate (lista_contatos, start=1):
-        favorito = "Favorito" if lista["Favorito"] else " "
-        print(f'{indice}. {lista["Nome"]} - {lista["Telefone"]} - {favorito}')
+def adicionar_contatos(lista_contatos, nome, telefone, email):
+    contato = {"Nome": nome, "Telefone": telefone, "Email": email, "Favorito": False}
+    lista_contatos.append(contato)
+    print(f"\nContato de {nome} adicionado com sucesso!")
     return
 
 def editar_contato(lista_contatos, indice_contato):
     indice_contato_ajustado = int(indice_contato) - 1
-    if indice_contato_ajustado >= 0 and indice_contato_ajustado < len(lista_contatos):        
-        print("\n Qual campo deseja alterar?")
-        print("1. Nome")
-        print("2. Telefone")
-        print("3. Email")
-        print("4. Favorito")
+    print("1. Nome")
+    print("2. Telefone")
+    print("3. Email")
+    
+    campo = input("\nQual campo deseja alterar?")
 
-        campo = input("Qual campo deseja alterar?")
-        if campo == "1":
-            campo = "Nome"
-        elif campo == "2":
-            campo = "Telefone"
-        elif campo == "3":
-            campo = "Email"
-        elif campo == "4":
-            campo = "Favorito"
+    if campo == "1":
+        campo = "Nome"
+    elif campo == "2":
+        campo = "Telefone"
+    elif campo == "3":
+        campo = "Email"
+    
+    novo_valor = input(f"\nDigite o novo valor para o campo {campo}: ")
 
-        novo_valor = input(f"Digite o novo valor para o {campo}")
+    lista_contatos[indice_contato_ajustado][campo] = novo_valor
+    
+    print(f"\nCampo {campo} ajustado para {novo_valor} com sucesso")
+    
+    return
 
-        if campo == "Favorito":
-            novo_valor = False
+def marcar_favorito(lista_contatos, indice_contato):
+    indice_contato_ajustado = int(indice_contato) - 1
+    print("\nO que deseja fazer?")
+    print("\n1. Marcar contato como favorito")
+    print("2. Desmarcar contato como favorito")
 
-        lista_contatos[indice_contato_ajustado][campo] = novo_valor
-
-        print("Contato atualizado com sucesso")
-
-    else:
-        print("Indice nao existe")
+    opcao = input("\nSelecione uma opção: ")
+    
+    if opcao == "1":
+        lista_contatos[indice_contato_ajustado]["Favorito"] = True
+    elif opcao == "2":
+        lista_contatos[indice_contato_ajustado]["Favorito"] = False
 
     return
 
-def excluir_contato(lista_contatos, indice_contato):
+def visualizar_favoritos(lista_contatos):
+    for indice, contato in enumerate (lista_contatos, start=1):
+        favorito = "★"
+        if contato["Favorito"]:
+            print(f'{indice}.  {contato["Nome"]} - {contato["Telefone"]} - {contato["Email"]} - {favorito}')
+    return
+
+def apagar_contato(lista_contatos, indice_contato):
     indice_contato_ajustado = int(indice_contato) - 1
     lista_contatos.pop(indice_contato_ajustado)
     return
 
-def marcar_favorito(indice_contato, lista_contatos):
-    indice_contato_ajustado = int(indice_contato) - 1
-    if indice_contato_ajustado >= 0 and indice_contato_ajustado < len(lista_contatos):  
-        lista_contatos[indice_contato_ajustado]["Favorito"] = True
-        print("Contato marcado como favorito")
-    else:
-        print("Indice nao existe")
-    return
-
-def contatos_favoritos(lista_contatos):
-    print("\nLista de contatos favoritos:")
-
-    for indice, contato in enumerate(lista_contatos, start=1):
-
-        if contato["Favorito"] == True:
-
-            print(f'{indice}. {contato["Nome"]} - {contato["Telefone"]}')
-
-
 lista_contatos = []
 
-while True:
-    print("\nBem vindo a sua agenda de contatos.")
+while True :
     print("\n1. Visualizar lista de contatos")
     print("2. Adicionar contato")
     print("3. Editar contato")
-    print("4. Excluir contato")
-    print("5. Marcar contato como favorito")
-    print("6. Lista de contatos favoritos")
+    print("4. Deletar contato")
+    print("5. Marcar/Desmarcar contato como favorito")
+    print("6. Visualizar lista de contatos favoritos")
     print("7. Sair")
-    escolha = input("\nSelecione uma opção para seguir:")
+
+    escolha = input("\n Selecione a opção desejada: ")
 
     if escolha == "1":
         visualizar_contatos(lista_contatos)
-    
+
     elif escolha == "2":
         nome = input("\nDigite o nome do contato: ")
-        telefone = input("\nDigite o telefone do contato: ")
-        email = input("\nDigite o email do contato: ")
-        adicionar_contatos(nome, telefone, email, lista_contatos)
+        telefone = input("Digite o telefone do contato: ")
+        email = input("Digite o email do contato: ")
+        adicionar_contatos(lista_contatos, nome, telefone, email)
 
     elif escolha == "3":
-        indice_contato = input("Qual o indice do contato que voce deseja alterar?")
+        indice_contato = input("Qual o índice do contato que deseja editar?")
         editar_contato(lista_contatos, indice_contato)
 
     elif escolha == "4":
-        indice_contato = input("Qual o indice do contato que voce deseja excluir?")
-        excluir_contato(indice_contato, lista_contatos)
+        indice_contato = input("Qual o índice do contato que deseja apagar?")
+        apagar_contato(lista_contatos, indice_contato)
 
     elif escolha == "5":
-        indice_contato = input("Qual o indice do contato que voce deseja marcar como favorito?")
-        marcar_favorito(indice_contato,lista_contatos)
+        indice_contato = input("Qual o índice do contato que deseja marcar/desmarcar como favorito?")
+        marcar_favorito(lista_contatos, indice_contato)
 
     elif escolha == "6":
-        contatos_favoritos(lista_contatos)
-    
+        visualizar_favoritos(lista_contatos)
+
     elif escolha == "7":
         print("\nPrograma encerrado!")
         break
